e7e11045a26fa1d92aa6fca7dc95c0cc9c1cd052
smasa <smasa@teknei.com>
2026-07-02 10:08:52 +0200

    Añadir comentario en build

--- scripts/build_data.py
+++ scripts/build_data.py
@@ -1,3 +1,15 @@
+import sdg.ProgressMeasure
+from sdg.ProgressMeasure import SeriesProgress
 from sdg.open_sdg import open_sdg_build
+
+
+class SeriesProgressEustat(SeriesProgress):
+    def __init__(self, indicator, config={}, logging=None):
+        print("[EUSTAT] >>> Motor de progreso personalizado de Eustat ACTIVO <<<")
+        super().__init__(indicator, config=config, logging=logging)
+
+
+# Monkey patching: reemplazar SeriesProgress por la versión Eustat
+sdg.ProgressMeasure.SeriesProgress = SeriesProgressEustat
 
 open_sdg_build(config='config_data.yml')
